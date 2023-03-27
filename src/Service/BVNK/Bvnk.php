37d30b5ddf4920ddd362ce71ea22331233a9d8b6
Hawkeye <swarup@agpaytech.co.uk>
2023-03-27 17:35:52 +0530

    methods added for header logic, exchange rate method modified in bvnk service class

--- src/Service/BVNK/Bvnk.php
+++ src/Service/BVNK/Bvnk.php
@@ -6,35 +6,97 @@ namespace APUHawkeye\BVNKAPI\Service\BVNK;
 use APUHawkeye\BVNKAPI\Service\ExchangeRateInterface;
 use APUHawkeye\BVNKAPI\Service\Model\ExchangeRate;
 use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Response;
 
 class Bvnk implements ExchangeRateInterface
 {
     public const BASE_URI = 'https://api.sandbox.bvnk.com';
 
     /**
-     * @param string $sourceCurrency
-     * @return \APUHawkeye\BVNKAPI\Service\Model\ExchangeRate
-     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @param int $length
+     * @return string
      */
-    public function exchangeRate(string $sourceCurrency): \APUHawkeye\BVNKAPI\Service\Model\ExchangeRate
+    protected function generateNonce(int $length = 6): string
     {
+        $chars = '1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM';
+        $charLength = strlen($chars) - 1;
+        $result = '';
+        while (strlen($result) < $length) {
+            $result .= $chars[rand(0, $charLength)];
+        }
+
+        return $result;
+    }
+
+    /**
+     * @param $string
+     * @param $key
+     * @return string
+     */
+    protected function generateSignature($string, $key): string
+    {
+        return base64_encode(
+            hash_hmac('sha256', $string, $key, true)
+        );
+    }
+
+    /**
+     * @return string
+     */
+    public function generateHeader(): string
+    {
+        $authId = '';
+        $authKey = '';
+        $time = time();
+        $nonce = $this->generateNonce();
+
+        $hawkHeader = "hawk.1.header"."\n";
+        $hawkHeader .= $time;
+        $hawkHeader .= "\n";
+        $hawkHeader .= $nonce;
+        $hawkHeader .= "\n";
+        $hawkHeader .= "GET";
+        $hawkHeader .= "\n";
+        $hawkHeader .= "/api/currency/values";
+        $hawkHeader .= "\n";
+        $hawkHeader .= "api.sandbox.bvnk.com";
+        $hawkHeader .= "\n";
+        $hawkHeader .= "443";
+        $hawkHeader .= "\n";
+        $hawkHeader .= "\n";
+        $hawkHeader .= "\n";
+
+        $signature = $this->generateSignature($hawkHeader, $authKey);
+
+        $header = "Hawk id=\"" .$authId."\"".","
+            ." ts=\"".$time."\"".","
+            ." nonce=\"".$nonce."\"".","
+            ." mac=\"".$signature."\"";
+
+        return $header;
+    }
+
+
+    public function exchangeRate(string $sourceCurrency): array
+    {
+        $exchangeRate = new ExchangeRate();
+        $exchangeRate->setBaseCurrency($sourceCurrency);
         $client = new Client();
-        $endpoint = self::BASE_URI . '/api/currency/values/' . $sourceCurrency . '?' . http_build_query([
-            'all' => true,
-        ]);
+        $header = $this->generateHeader();
+        $endpoint = self::BASE_URI . '/api/currency/values/' . $sourceCurrency . '?all=true' ;
+        /** @var Response $response */
         $response = $client->request('GET', $endpoint, [
-            'headers' => [
-                'Authorization' => '',
-                'accept' => 'application/json',
-            ]
+            'headers' => $header
         ]);
         if ($response->getStatusCode() ===  200) {
-            $json = json_decode($response->getBody()->getContents(), true);
-            $exchangeRate = new ExchangeRate();
-            $exchangeRate->setExchangeRate($json['rate']);
-            $exchangeRate->setToCurrency($json['to_currency']);
-
-            return $exchangeRate;
+            /** @var array $responseBody */
+            $responseBody = json_decode($response->getBody()->getContents(), true);
+            foreach ($responseBody as $body) {
+                $exchangeRate = new ExchangeRate();
+                $exchangeRate->setBaseCurrency($sourceCurrency);
+                $exchangeRate->setCounterCurrency($body['counterCode']);
+                $exchangeRate->setExchangeRate($body['rate']);
+            }
         }
     }
 }
