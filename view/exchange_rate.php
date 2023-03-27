60cde713d571780eb3065ebd96b2fcf4dadbd520
Hawkeye <swarup@agpaytech.co.uk>
2023-03-27 20:32:35 +0530

    bug fix

--- view/exchange_rate.php
+++ view/exchange_rate.php
@@ -5,7 +5,9 @@ require_once "../vendor/autoload.php";
 $bvnk = new \APUHawkeye\BVNKAPI\Service\BVNK\Bvnk();
 
 try {
-    $rate = $bvnk->exchangeRate('BTC', 'INR', 1);
+    $rate = $bvnk->exchangeRate('BTC');
     $amount = $bvnk->covertAmount('GBP', 'BTC', 1500);
 } catch (\GuzzleHttp\Exception\GuzzleException $e) {
+    echo 'ERROR: ' . $e->getMessage();
+    return;
 }
