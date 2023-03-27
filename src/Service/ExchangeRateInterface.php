a49157b3c376863fbf39dd7cf054242d5453abc5
Hawkeye <swarup@agpaytech.co.uk>
2023-03-27 19:58:25 +0530

    interface modified

--- src/Service/ExchangeRateInterface.php
+++ src/Service/ExchangeRateInterface.php
@@ -7,5 +7,7 @@ use APUHawkeye\BVNKAPI\Service\Model\ExchangeRate;
 
 interface ExchangeRateInterface
 {
-    public function exchangeRate(string $sourceCurrency): ?ExchangeRate;
+    public function exchangeRate(string $sourceCurrency): array;
+
+    public function covertAmount(string $sourceCurrency, string $counterCurrency, float $sourceAmount): float;
 }
