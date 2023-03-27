a8ebd240672e1e3ed427d89c6d3675ba17f5124b
Hawkeye <swarup@agpaytech.co.uk>
2023-03-27 19:18:41 +0530

    model returned

--- src/Service/ExchangeRateInterface.php
+++ src/Service/ExchangeRateInterface.php
@@ -3,7 +3,9 @@ declare(strict_types=1);
 
 namespace APUHawkeye\BVNKAPI\Service;
 
+use APUHawkeye\BVNKAPI\Service\Model\ExchangeRate;
+
 interface ExchangeRateInterface
 {
-    public function exchangeRate(string $sourceCurrency): array;
+    public function exchangeRate(string $sourceCurrency): ?ExchangeRate;
 }
